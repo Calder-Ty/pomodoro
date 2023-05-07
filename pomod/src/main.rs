1b993446f61bb5ecbc966f80d2ad22bb6a4cca08
Tyler Calder <calder-ty@protonmail.com>
2023-05-06 21:35:15 -0600

    feat: Add A simple Protocol

--- pomod/src/main.rs
+++ pomod/src/main.rs
@@ -1,13 +1,12 @@
+use std::fs::remove_file;
+use std::io::{Read, Result};
+use std::os::unix::net::UnixListener;
 use std::process::exit;
 use std::time::Instant;
-use std::io::{Result, Read};
-use std::os::unix::net::UnixListener;
-use std::fs::remove_file;
 
-const POMO_SOCKET: &str =  "/var/run/pomod.sock";
+const POMO_SOCKET: &str = "/var/run/pomod.sock";
 
-fn main() -> Result<()>{
-
+fn main() -> Result<()> {
     let _ = ctrlc::set_handler(|| {
         eprintln!("Recieved Keyboard Interupt!");
         match remove_file(POMO_SOCKET) {
@@ -19,13 +18,16 @@ fn main() -> Result<()>{
 
     let socket = UnixListener::bind(POMO_SOCKET)?;
 
+    let mut app: Option<Session> = None;
+
     for stream in socket.incoming() {
         match stream {
             Ok(mut s) => {
                 // Handle The Input and Route it to actions
-                let mut input_buf = String::new();
-                s.read_to_string(&mut input_buf)?;
-                dbg!(input_buf);
+                let mut input_buf = vec![];
+                s.read_to_end(&mut input_buf)?;
+                dbg!(&input_buf);
+                handle_request(&input_buf[0..5]);
             }
             Err(_) => {
                 eprintln!("Socket Connection Failed")
@@ -35,6 +37,33 @@ fn main() -> Result<()>{
     Ok(())
 }
 
+/// Parse Request
+///
+/// Pomod Protocol: 1 byte -> Command, 4 bytes-> Optional data to use. If command doesn't
+/// Take additional data, then push only 0x0
+/// And then route it to handler
+fn handle_request(request: &[u8]) {
+    let final_bytes = request[1..=4].try_into().expect("Oopsie");
+
+    match request[0] {
+        1_u8 => println!(
+            "Recieved Start Request for {} Seconds",
+            u32::from_be_bytes(final_bytes)
+        ),
+        2_u8 => todo!("Handle Stop"),
+        3_u8 => todo!("Handle Status"),
+        _ => eprintln!("Will Not handle Faulty request"),
+    }
+}
+
+/// What we want to do with the Pomodoro
+#[derive(Debug, Clone, Copy)]
+#[repr(i8)]
+enum Commands {
+    Start(u32) = 1,
+    Stop = 2,
+    Status = 3,
+}
 
 #[derive(Debug, Clone, Copy)]
 struct Session {
@@ -43,21 +72,19 @@ struct Session {
 }
 
 impl Default for Session {
-
     fn default() -> Self {
-        Self { status: SessionStatus::Working, current_chunk: TimeChunk::new(25 * 60) }
+        Self {
+            status: SessionStatus::Working,
+            current_chunk: TimeChunk::new(25 * 60),
+        }
     }
-
 }
-
-
 
 #[derive(Debug, Clone, Copy)]
-enum SessionStatus{
+enum SessionStatus {
     Working,
-    Resting
+    Resting,
 }
-
 
 /// A TimeChunk represents a set of time
 /// That is used keep track.
